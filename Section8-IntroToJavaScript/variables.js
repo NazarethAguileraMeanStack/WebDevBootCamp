64ce65fbe4ffa3254c8c67409872885d58a5918e
Nazareth Aguilera <NazarethAguileraMeanStack@gmail.com>
2018-06-11 12:55:46 -0700

    Intro to JavaScript variables and methods

---
+++ Section8-IntroToJavaScript/variables.js
@@ -0,0 +1,43 @@
+
+let name = "Nazareth";
+console.log(name);
+let age = 26;
+console.log(age);
+let isWorkingHard = true;
+console.log(isWorkingHard);
+
+let greeting = `Hello there, ${name}`;
+console.log(greeting);
+let questionAge = `Are you really ${age} years old?`;
+console.log(questionAge);
+let evaluation = `It is ${isWorkingHard} that you worked hard this week`;
+console.log(evaluation);
+
+let ageIn5Years = age + 5;
+console.log(ageIn5Years);
+
+let iCanBeAnything = 1;
+console.log(iCanBeAnything);
+iCanBeAnything = "Hello";
+console.log(iCanBeAnything);
+iCanBeAnything = true;
+console.log(iCanBeAnything);
+iCanBeAnything = null;
+console.log(iCanBeAnything);
+iCanBeAnything = undefined;
+console.log(iCanBeAnything);
+iCanBeAnything = 3.9;
+console.log(iCanBeAnything);
+
+
+//Methods!
+
+//console.log("Prints in the console!");
+//alert("Hello there! (Very Intrusive!)");
+//prompt("Gets an input string from the user!");
+
+
+//let userName = prompt("What is your name?");
+//alert(`Nice to meet you ${userName}`);
+//console.log(User's Name is: `${userName}`);
+
