10cdd7715b59f4a1ded679a37149f75f56f5a9f0
Nazareth Aguilera <NazarethAguileraMeanStack@gmail.com>
2018-06-11 14:24:04 -0700

    Basic JavaScript Exercise 1

---
+++ Section8-IntroToJavaScript/Exercise1.js
@@ -0,0 +1,10 @@
+
+
+
+let firstName = prompt("What is your first name?");
+let lastName = prompt("What is your last name?");
+let userAge = prompt("What is your age?");
+
+alert(`Your full name is ${firstName} ${lastName}`);
+alert(`You are ${userAge} years old.`);
+
