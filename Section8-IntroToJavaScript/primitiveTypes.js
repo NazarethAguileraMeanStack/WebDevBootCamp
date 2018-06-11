9367af5e5136dcdec157c1e242d92e5f18291f47
Nazareth Aguilera <NazarethAguileraMeanStack@gmail.com>
2018-06-11 12:41:23 -0700

    Intro to JavaScript. Primitive Types

---
+++ Section8-IntroToJavaScript/primitiveTypes.js
@@ -0,0 +1,44 @@
+
+
+
+//Numbers
+4
+9.3
+-10
+
+//Strings
+"Hello World"
+"43"
+
+//Booleans
+true
+false
+
+//null and undefined
+null
+undefined
+
+
+//Basic Math
+4 + 10 //14
+1/5 //0.2
+6 - 3 //3
+1 * 8 //8
+(3-8) * 24 //-120
+
+//Modulo
+10 % 3 //1
+24 % 2 //0
+15 % 11 //5
+
+
+//strings
+"hello class" //hello class
+'dogs are awesome' //dogs are awesome
+" I can't stop eating candy" //I can't stop eating candy
+"Help" + " Me" // Help Me
+"she said \"goodbye!\"" //she said "goodbye!"
+"This is backslash \\" //This is backslash \
+"my favorite number is : 1234".length // 27
+"The Beatles"[0] //T
+"The Beatles"["The Beatles.length - 1"] //s
