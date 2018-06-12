bdf05b653263f3dccd17a3c7e036aee512f3516a
Nazareth Aguilera <NazarethAguileraMeanStack@gmail.com>
2018-06-11 17:40:54 -0700

    Intro to Arrays

---
+++ Section11-IntroToArrays/arrays.js
@@ -0,0 +1,36 @@
+
+
+
+// An Array is a Data Structure which describes how we can hold data together.
+
+/*Suppose we wanted to model a group of friends:
+let friend1 = "Tim";
+let friend2 = "John";
+let friend3 = "Sally";
+let friend4 = "Rebecca";
+
+THIS IS NOT DRY
+
+use an array:
+
+let friends = ['Tim', 'John', 'Sally', 'Rebecca'];
+
+friends[0] = Tim
+friends[1] = John
+friends[2] = Sally
+friends[3] = Rebecca
+
+Always start from 0 index.
+
+
+*/
+
+let friends = ["Tim", "john"];
+console.log(friends);
+friends[0] = "Jake";
+console.log(friends);
+
+
+let arrays = [4, "Helloo", true, null, 4.4];
+console.log(arrays);
+console.log(arrays.length);
