0942f8ed8494d62ce3145c7928f66fe347944f61
Nazareth Aguilera <NazarethAguileraMeanStack@gmail.com>
2018-06-14 12:15:59 -0700

    jQuery Practice Exercise

---
+++ Section16-jQuery/jQueryExercise.js
@@ -0,0 +1,19 @@
+
+
+
+/*
+jQuery Exercise
+
+1. Correctly include jQuery
+2. Select all divs and give them a purple background.
+3. Select the divs with class "highlight" and make them 200px wide
+4. Select the div with id "third" and give it a orange border
+Bonus: Select the first div only and change its font color to pink
+
+*/
+
+
+$("div").css("backgroundColor", "purple");
+$(".highlight").css("width", "200px");
+$("#third").css("border", "2px solid orange");
+$("div").first().css("color", "pink");
