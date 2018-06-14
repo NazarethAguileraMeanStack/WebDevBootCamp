e041189f29ac44c2092170e8a3e7bf2156f32325
Nazareth Aguilera <NazarethAguileraMeanStack@gmail.com>
2018-06-14 13:09:31 -0700

    Practicing jQuery Methods

---
+++ Section16-jQuery/jQueryMethods.js
@@ -0,0 +1,15 @@
+
+
+
+$("h1").text("New Text!!!!");
+$("ul").html("<li>i hacked your list!</li><li>Rusty is still adorable!</li>");
+
+$("img").attr("src", "https://vignette.wikia.nocookie.net/powerlisting/images/0/06/Ahri_the_Nine-Tailed_Fox_%28League_of_Legends%29.jpg/revision/latest/scale-to-width-down/640?cb=20171217133704");
+
+//$("input").attr("type", "color");
+
+$("h1").addClass("correct")
+$("li").addClass("wrong");
+
+
+$("li").toggleClass("correct");
