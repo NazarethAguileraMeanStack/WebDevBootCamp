d0424cf458646435f1d391270e2e21e292a1fbb3
Nazareth Aguilera <NazarethAguileraMeanStack@gmail.com>
2018-06-14 12:03:15 -0700

    Intro to jQuery

---
+++ Section16-jQuery/jQueryDemo.js
@@ -0,0 +1,16 @@
+$("h1").css("color", "pink");
+
+$("li").css("color", "yellow")
+
+$("#adorable").css("color", "red");
+
+
+let stylesObject = {
+    color: "red",
+    backgroundColor: "pink",
+    border: "2px solid blue",
+    fontSize: "50px",
+};
+
+
+$("h1").css(stylesObject);
